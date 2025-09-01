642714bdf46e1f87b0a087cd29dfdbc88209c9a3
Nathaniel Tampus <nate@mugnavo.com>
2025-09-02 02:41:12 +0800

    update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
 
     // https://nextjs.org/docs/app/api-reference/config/next-config-js/reactCompiler
     reactCompiler: true,
+
+    clientSegmentCache: true,
   },
 };
 
