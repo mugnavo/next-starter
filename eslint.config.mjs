1dc6e2b3ced2eb99c4db1f859fb6e81480e35986
Nathaniel Tampus <nate@mugnavo.com>
2025-09-07 17:39:22 +0800

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,9 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginQuery from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettier from "eslint-config-prettier";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
