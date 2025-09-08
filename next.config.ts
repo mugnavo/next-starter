dceffdf87bbd0062d5f9c52e80117c69063c4b4d
Nathaniel Tampus <nate@mugnavo.com>
2025-09-08 22:24:41 +0800

    add experimental staleTimes

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,12 @@ const nextConfig: NextConfig = {
     // https://nextjs.org/docs/app/api-reference/config/next-config-js/reactCompiler
     reactCompiler: true,
 
+    // https://nextjs.org/docs/app/api-reference/config/next-config-js/staleTimes
+    staleTimes: {
+      dynamic: 60, // 1 minute
+      static: 300, // 5 minutes
+    },
+
     clientSegmentCache: true,
   },
 };
